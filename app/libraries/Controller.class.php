58a1af5cc51be9bf1c5ecef34a7a437e24573dcd
MrLonelly <apavalac@student.42.fr>
2020-09-11 21:08:05 +0300

    Finished Core class

---
+++ app/libraries/Controller.class.php
@@ -0,0 +1,17 @@
+<?php
+
+    /* 
+     * Base controller
+     * Loads model and views
+     * 
+     */
+
+    Class Controller
+    {
+        public function model($model)
+        {
+            
+        }
+    }
+
+?>
