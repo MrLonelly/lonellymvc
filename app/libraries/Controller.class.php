52fe480e232c136867c1107e4e00db712fd90249
MrLonelly <apavalac@student.42.fr>
2020-09-11 21:18:57 +0300

    Controller class

--- app/libraries/Controller.class.php
+++ app/libraries/Controller.class.php
@@ -8,9 +8,27 @@
 
     Class Controller
     {
+        // Load model
         public function model($model)
         {
-            
+            // Require model file
+            require_once '../models/' . $model . '.class.php';
+
+            return new $model();
+        }
+
+        // Load view
+        public function view($view, $data = [])
+        {
+            // Check for the view file
+            if(file_exists('../app/views/' . $view . '.php'))
+            {
+                require_once '../app/views/' . $view . '.php';
+            }
+            else
+            {
+                require_once '../app/views/404.html';
+            }
         }
     }
 
